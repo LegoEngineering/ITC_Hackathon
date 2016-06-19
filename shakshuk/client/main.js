b7d71e3df2b6fda2db8aef4b2d440db7dfba89d4
erebibo <esteerebibo@gmail.com>
2016-06-19 23:02:14 +0300

    Update main.js

--- shakshuk/client/main.js
+++ shakshuk/client/main.js
@@ -3,11 +3,21 @@ Template.main.helpers({
   "setMain":function(){
     Session.set("filter", null);
     Session.set("storeRate", "10");
+    Session.set("view", "Map");
   },
   "loggedIn":function(){
-    console.log("hi");
     return Meteor.user();
+  },
+  "showMap":function(){
+    return Session.equals("view", "Map");
+  },
+  "showList":function(){
+    return Session.equals("view", "List");
+  },
+  "showComments":function(){
+    return Session.equals("view", "Comments");
   }
+
 });
 
 Template.main.events({
@@ -22,6 +32,28 @@ Template.main.events({
 
 });
 
+Template.mapView.onRendered(function initMap() {
+    var myLatLng = {lat: 32.06868, lng: 34.76905};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 18,
+      center: myLatLng
+    });
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Hello World!',
+      id: 1
+    });
+    marker.addListener("click", function(){
+      console.log("hi");
+    });
+
+});
+Template.mapView.events({
+  "click #goToList": function(){
+    Session.set("view", "List");
+  }
+});
 Template.rate.helpers({
   "selectedStore":function(){
     return Stores.findOne({_id: Session.get("storeRate")});
@@ -50,10 +82,32 @@ Template.listView.helpers({
 });
 
 Template.listView.events({
+  "click #goToMap": function(){
+    Session.set("view", "Map");
+  },
   "change #filter":function(e){
     var filter = $(e.target).val();
     console.log(filter);
     Session.set("filter", filter);
+  },
+  "click .viewComments":function(){
+    Session.set("storeRate", this._id);
+    Session.set("view", "Comments");
+  }
+});
+
+Template.commentView.helpers({
+  "comments": function(){
+    return Comments.find({storeID: Session.get("storeRate")});
+  },
+  "user":function(){
+  return Meteor.users.findOne({_id: this.userID}).username;
+}
+});
+
+Template.commentView.events({
+  "click #goToList": function(){
+    Session.set("view", "List");
   }
 });
 
